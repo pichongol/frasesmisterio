e4bff0d184d820066bd218aaf8fadbe1579d65ac
pichongol <hoygan.ventures@gmail.com>
2015-11-27 21:32:51 -0300

    first commit

--- testlist_es.php
+++
@@ -1,9 +0,0 @@
-  <ul>
-    <? foreach ($testList as $name) { ?>
-    <li>
-      <article>
-        <figure><a href="http://<?=$domain?>/<?=$name?>/"><img style="border:8px solid #f3f3f3; max-width: 100%; margin-bottom:10px;" src="/images/<?=$language?>/<?=$name?>/m.png"></a></figure>
-      </article>
-    </li>
-    <? } ?>
-  </ul>
--- testlist_pt.php
+++
@@ -1,7 +0,0 @@
-<div id="preg1">  
-  <a href="/sexy/"><img style="border:8px solid #f3f3f3; max-width: 100%; margin-bottom:10px;" src="/images/pt/sexy/m.png"></a>
-  <br />
-  <a href="/falam/"><img style="border:8px solid #f3f3f3; max-width: 100%; margin-bottom:10px;" src="/images/pt/falam/m.png"></a>
-  <br />
-  <a href="/casal/"><img style="border:8px solid #f3f3f3; max-width: 100%; margin-bottom:10px;" src="/images/pt/casal/m.png"></a>
-</div>

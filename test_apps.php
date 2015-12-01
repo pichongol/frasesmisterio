3fddd93cd98f065d9204592bb69074ca93fc1f90
pichongol <hoygan.ventures@gmail.com>
2015-12-01 10:47:41 -0300

    poster

--- test_apps.php
+++ test_apps.php
@@ -7,6 +7,10 @@ use Facebook\FacebookRedirectLoginHelper;
 use Facebook\FacebookRequest;
 use Facebook\GraphUser;
 
+if( !isset($_SESSION['access_token']) && !isset($_GET['code']) ){
+	die(header("Location: /"));
+}
+
 // mysql
 $dbConn = @mysql_connect($host, $user, $password);
 mysql_select_db($database,$dbConn);

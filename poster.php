1bc89072f8883f9eb694ca8b3ff15a814fb34033
pichongol <hoygan.ventures@gmail.com>
2015-12-01 10:28:18 -0300

    poster

---
+++ poster.php
@@ -0,0 +1,46 @@
+<?
+include("config.php");
+
+// mysql
+$dbConn = @mysql_connect($host, $user, $password);
+mysql_select_db($database,$dbConn);
+mysql_set_charset("utf8",$dbConn);
+
+$sql = "SELECT * FROM users WHERE ((last_post_date IS NULL) || last_post_date != DATE(NOW()))";
+$r_select = mysql_query($sql, $dbConn) or die(mysql_error($dbConn));
+
+while( $user = mysql_fetch_array($r_select) ){
+	$imgs = scandir($img_path);
+	sort($imgs);
+
+	$img = $imgs[rand(3,count($imgs)-1)];
+	$img = str_replace(".jpg", "", $img);
+
+	$post = array (
+		"access_token" => $user['access_token'],
+		"message" => "",
+		"name" => "La Frase del Dia",
+		"link" => "http://www.frasesmisterio.com/frase/{$img}",
+		"picture" => "http://www.frasesmisterio.com/images/es/{$img}.jpg",
+		"description" => "Frases que Inspiran...",
+		"caption" => "",
+	);
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+    curl_setopt($ch, CURLOPT_URL, "https://graph.facebook.com/v2.2/{$user['uid']}/feed");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+    $result = curl_exec($ch);
+    $error = curl_error($ch);
+    curl_close($ch);
+    unset($ch);
+
+	$sql = "UPDATE IGNORE shares_{$type} SET post_title = '{$posteable['title']}', post_permalink = '{$posteable['permalink']}', description = '{$description}', picture = '{$posteable['picture']}', video_id = {$posteable['id']} WHERE id = {$data['id']}";
+    mysql_query($sql, $dbConn) or die(mysql_error($dbConn));
+
+}
+?>
